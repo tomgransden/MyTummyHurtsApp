ffaf3a2821c50c7a92c7ce85e18b95409f79f5ca
Thomas Gransden <thomasgransden@gmail.com>
2023-04-15 12:11:52 +0100

    Medication screen behaviour

--- src/components/MedicationScreen/MedicationScreen.tsx
+++ src/components/MedicationScreen/MedicationScreen.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 
+import { RootStackParamList } from '../../../App';
 import styles from './MedicationScreen.style';
 import Pill from './subcomponents/Pill/Pill';
 
-const MedicationScreen = () => (
-  <View style={styles.container}>
-    <View style={styles.pillList}>
-      {['Paracetamol 400mg', 'Buscopan 20mg', 'Lixana 100mg'].map((element) => (
-        <Pill key={element} name={element} />
-      ))}
+type MedicationScreenProps = NativeStackScreenProps<RootStackParamList, 'Medication'>;
+
+const MedicationScreen = ({ navigation }: MedicationScreenProps) => {
+  const [medication, addMedication] = useState<string[]>([
+    'Paracetamol 400mg',
+    'Buscopan 20mg',
+    'Lixana 100mg',
+  ]);
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <AntDesign
+          onPress={() => addMedication((prevState) => [...prevState, 'a'])}
+          name="pluscircleo"
+          size={24}
+          color="white"
+        />
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.pillList}>
+        {medication.map((element) => (
+          <Pill key={element} name={element} />
+        ))}
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 export default MedicationScreen;
--- src/components/MenuScreen/MenuScreen.tsx
+++ src/components/MenuScreen/MenuScreen.tsx
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 
 import { RootStackParamList } from '../../../App';
