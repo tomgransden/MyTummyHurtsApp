d035f9c1e61e67c9059ae0223cc93368ab3943cd
Thomas Gransden <thomasgransden@gmail.com>
2023-04-15 21:57:24 +0100

    Add bottom sheet to medication screen

--- src/components/MedicationScreen/MedicationScreen.tsx
+++ src/components/MedicationScreen/MedicationScreen.tsx
@@ -1,26 +1,35 @@
 import { AntDesign } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
+import { View, Text } from 'react-native';
 
 import { RootStackParamList } from '../../../App';
 import styles from './MedicationScreen.style';
 import Pill from './subcomponents/Pill/Pill';
+import BottomSheet, { BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import { TextInput } from 'react-native-gesture-handler';
 
 type MedicationScreenProps = NativeStackScreenProps<RootStackParamList, 'Medication'>;
 
 const MedicationScreen = ({ navigation }: MedicationScreenProps) => {
-  const [medication, addMedication] = useState<string[]>([
-    'Paracetamol 400mg',
-    'Buscopan 20mg',
-    'Lixana 100mg',
-  ]);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const inputRef = useRef<TextInput>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+    //if (index === 0) inputRef.current?.focus();
+  }, []);
+  const [medication, addMedication] = useState<string[]>([]);
 
   React.useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <AntDesign
-          onPress={() => addMedication((prevState) => [...prevState, 'a'])}
+          onPress={() => bottomSheetRef.current?.expand()}
           name="pluscircleo"
           size={24}
           color="white"
@@ -31,11 +40,42 @@ const MedicationScreen = ({ navigation }: MedicationScreenProps) => {
 
   return (
     <View style={styles.container}>
+      {!medication.length ? (
+        <Text
+          style={{ fontFamily: 'Rubik', fontSize: 24, fontWeight: 'bold', textAlign: 'center' }}>
+          No medications available
+        </Text>
+      ) : null}
       <View style={styles.pillList}>
         {medication.map((element) => (
           <Pill key={element} name={element} />
         ))}
       </View>
+      <BottomSheet
+        enablePanDownToClose={true}
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        keyboardBehavior="interactive"
+        keyboardBlurBehavior="restore"
+        onChange={handleSheetChanges}>
+        <View style={{ marginHorizontal: 16 }}>
+          <BottomSheetTextInput
+            ref={inputRef}
+            placeholder={'Enter name/dosage'}
+            style={{ fontSize: 24, fontFamily: 'Rubik' }}
+            onEndEditing={(e) => {
+              if (e.nativeEvent.text.length)
+                addMedication((prevMeds) => [...prevMeds, e.nativeEvent.text]);
+
+              setTimeout(() => {
+                bottomSheetRef.current?.close();
+                inputRef.current?.clear();
+              }, 50);
+            }}
+          />
+        </View>
+      </BottomSheet>
     </View>
   );
 };
