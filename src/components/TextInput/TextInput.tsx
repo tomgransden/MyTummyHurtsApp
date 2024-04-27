e29e50098e5c2410e307ec5b7814ba7f0509b30f
Thomas Gransden <thomas.gransden@petsathome.co.uk>
2024-04-27 19:42:30 +0100

    Tidy up components

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,24 +1,9 @@
 import { TextInputProps, TextInput as TextInputBase } from 'react-native';
 
+import { styles } from './TextInput.style';
+
 const TextInput = ({ style, ...rest }: TextInputProps) => {
-  return (
-    <TextInputBase
-      placeholderTextColor="white"
-      style={[
-        {
-          color: 'white',
-          backgroundColor: '#bfa2c8',
-          height: 56,
-          fontSize: 20,
-          borderRadius: 24,
-          paddingHorizontal: 12,
-          fontFamily: 'Rubik',
-        },
-        style,
-      ]}
-      {...rest}
-    />
-  );
+  return <TextInputBase placeholderTextColor="white" style={[styles.container, style]} {...rest} />;
 };
 
 export default TextInput;
